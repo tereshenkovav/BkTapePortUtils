aee35d879c98cdd700b8c908d13acfb7949629df
TAV <tereshenkov@mail.ru>
2026-04-18 17:21:37 +0300

    fix: В скрипте сборки для FPC добавлена очистка выходного каталога.

--- build/make_win32.bat
+++ build/make_win32.bat
@@ -1,3 +1,6 @@
 mkdir i386-win32
+REM Очистка каталога важна, потому что в проектах одинаковые файлы main
+del i386-win32\* /q
 ppc386 @fpc-config ..\src\BkSendBasic\BkSendBasic.pp
+del i386-win32\* /q
 ppc386 @fpc-config ..\src\BkSendFocal\BkSendFocal.pp
